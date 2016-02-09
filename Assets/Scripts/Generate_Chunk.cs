3b727c79c6562a825043da72095142c0335df676
Yiyi Wang <shd101wyy@sina.com>
2016-02-08 23:01:23 -0600

    trying to support infinite terrain

---
+++ Assets/Scripts/Generate_Chunk.cs
@@ -0,0 +1,312 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum Type {GRASS, SAND, SNOW, DIRT};
+
+public class Block {
+	public Type type;
+	public bool visible;
+	public Vector3 pos;
+
+	public GameObject meshObject;
+
+	protected GameObject front;
+	protected GameObject back;
+	protected GameObject left;
+	protected GameObject right;
+	protected GameObject top;
+	protected GameObject bottom;
+
+	private GameObject clone;
+
+	public Block(Type type, bool visible, Vector3 pos) {
+		this.type = type;
+		this.visible = visible;
+		this.pos = pos;
+
+
+		front = null;
+		back = null;
+		left = null;
+		right = null;
+		top = null;
+		bottom = null;
+	}
+
+	public void setClone(GameObject clone) {
+		this.clone = clone;
+
+		GameObject top = (GameObject)clone.transform.FindChild ("Top").gameObject;
+		GameObject bottom = (GameObject)clone.transform.FindChild ("Bottom").gameObject;
+		GameObject left = (GameObject)clone.transform.FindChild ("Left").gameObject;
+		GameObject right = (GameObject)clone.transform.FindChild ("Right").gameObject;
+		GameObject front = (GameObject)clone.transform.FindChild ("Front").gameObject;
+		GameObject back = (GameObject)clone.transform.FindChild ("Back").gameObject;
+
+		addFaces (front, back, left, right, top, bottom);
+	}
+
+	public void addFaces( GameObject front, 
+		GameObject back,
+		GameObject left,
+		GameObject right,
+		GameObject top,
+		GameObject bottom) {
+		this.front = front;
+		this.back = back;
+		this.left = left;
+		this.right = right;
+		this.top = top;
+		this.bottom = bottom;
+	}
+
+	public void disableInvisibleFaces(Dictionary<Vector3, Block> chunk) {
+		front.SetActive (true);
+		back.SetActive (true);
+		left.SetActive (true);
+		right.SetActive (true);
+		top.SetActive (true);
+		bottom.SetActive (true);
+
+
+		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y, (int)pos.z - 1))) {
+			front.SetActive(false);
+		}
+		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y, (int)pos.z + 1))) {
+			back.SetActive(false);
+		}
+		if (chunk.ContainsKey (new Vector3 ((int)pos.x + 1, (int)pos.y, (int)pos.z))) {
+			// right.GetComponent<Renderer> ().enabled = false;
+			// Destroy(right);
+			right.SetActive(false);
+		}
+		if (chunk.ContainsKey (new Vector3 ((int)pos.x - 1, (int)pos.y, (int)pos.z))) {
+			// left.GetComponent<Renderer> ().enabled = false;
+			// Destroy(left);
+			left.SetActive(false);
+		}
+		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y - 1, (int)pos.z))) {
+			// bottom.GetComponent<Renderer> ().enabled = false;
+			// Destroy(bottom);
+			bottom.SetActive(false);
+		}
+	}
+}
+
+public class Grass: Block {
+	public Grass(bool visible, Vector3 pos) : base(Type.GRASS, visible, pos) {
+		this.meshObject = GameObject.Find("Grass");
+	}
+}
+
+public class Dirt: Block {
+	public Dirt(bool visible, Vector3 pos) : base(Type.DIRT, visible, pos) {
+		this.meshObject = GameObject.Find("Dirt");
+	}
+}
+
+public class Snow: Block {
+	public Snow(bool visible, Vector3 pos) : base(Type.SNOW, visible, pos) {
+		this.meshObject = GameObject.Find("Snow");
+	}
+}
+
+public class Sand: Block {
+	public Sand(bool visible, Vector3 pos) : base(Type.SAND, visible, pos) {
+		this.meshObject = GameObject.Find("Sand");
+	}
+}
+
+public class Generate_Chunk : MonoBehaviour {
+	private int width;
+	private int depth;
+	private int height;
+	private int chunkX;
+	private int chunkZ;
+
+	public int heightScale = 20;
+	public float detailScale = 25.0f;
+
+	Dictionary<Vector3, Block> worldBlocks = new Dictionary<Vector3, Block>();
+
+	private GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void startGeneratingChunk(int seed, int chunkWidth, int chunkDepth, int chunkHeight, int chunkX, int chunkZ,
+		GameObject player) {
+		this.width = chunkWidth;
+		this.depth = chunkDepth;
+		this.height = chunkHeight;
+		this.chunkX = chunkX;
+		this.chunkZ = chunkZ;
+		this.player = player;
+
+		// int seed = (int)Network.time * 10;
+		ArrayList visibleBlocks = new ArrayList ();
+
+		int startX = width * chunkX;
+		int startZ = depth * chunkZ;
+
+		for (int z = 0; z < depth; z++) {
+			for (int x = 0; x < width; x++) {
+				int y = (int)(Mathf.PerlinNoise ((x+seed+startX)/detailScale, (z+seed+startZ)/detailScale) * heightScale);
+				Vector3 blockPos = new Vector3 (x+startX, y, z+startZ);
+
+				visibleBlocks.Add(blockPos);
+				createBlock (blockPos, true);
+				while (y > 0) {
+					y--; 
+					blockPos = new Vector3 (x+startX, y, z+startZ);
+					createBlock (blockPos, false);
+				}
+			}
+		}
+
+		foreach (Vector3 blockPos in visibleBlocks) {
+			Block block = worldBlocks [blockPos];
+			block.visible = true;
+			drawBlock (block.type, blockPos);
+		}
+	}
+
+	void createBlock(Vector3 blockPos, bool isTop) {
+		int y = (int)blockPos.y;
+		Block block;
+		if (y > 15) {
+			block = new Snow (false, blockPos);
+		} else if (isTop && y > 5) {
+			block = new Grass (false, blockPos);
+		} else if (y > 5) {
+			block = new Dirt (false, blockPos);
+		} else {
+			block = new Sand (false, blockPos);
+		}
+
+		worldBlocks[blockPos] = block;
+	}
+
+	void drawBlock(Type type, Vector3 blockPos) {
+		Block block = worldBlocks [blockPos];
+		GameObject clone = (GameObject)Instantiate (block.meshObject, block.pos, Quaternion.identity);
+		block.setClone (clone);
+		clone.name = blockPos.ToString ();
+
+		block.disableInvisibleFaces (worldBlocks);
+
+	}
+
+	void drawInvisibleBlock(Vector3 blockPos) {
+		if (worldBlocks.ContainsKey (blockPos) == false)
+			return;
+
+
+		Block block = worldBlocks[blockPos];
+		Type type = block.type;
+		if (block.visible) {
+			block.disableInvisibleFaces (worldBlocks);
+			return;
+		}
+		block.visible = true; 
+		drawBlock (block.type, blockPos);
+	}
+
+	void drawAroundInvisibleBlocks(Vector3 blockPos) {
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				for(int z = -1; z <= 1; z++) {
+					if (!(x == 0 && y == 0 && z == 0)) {
+						Vector3 neighbour = new Vector3 ((int)blockPos.x + x, (int)blockPos.y + y, (int)blockPos.z + z);
+						drawInvisibleBlock (neighbour);
+					}
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// left click
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0));
+
+			if (Physics.Raycast (ray, out hit, 10.0f)) {
+
+				GameObject block = hit.transform.parent.gameObject;
+				if (!worldBlocks.ContainsKey (block.transform.position))
+					return;
+				
+				Vector3 blockPos = block.transform.position;
+
+				
+				// this is the bottom block, don't delete it 
+				if ((int)blockPos.y == 0) return;
+
+				worldBlocks.Remove(blockPos);
+				Destroy (block);
+
+				drawAroundInvisibleBlocks (blockPos);
+			}
+		}
+
+		// right click
+		if (Input.GetMouseButtonDown (1)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0));
+
+			if (Physics.Raycast (ray, out hit, 10.0f)) {
+
+				GameObject block = hit.transform.parent.gameObject;
+				if (!worldBlocks.ContainsKey (block.transform.position))
+					return;
+				
+				string faceName = hit.transform.gameObject.name;
+				Vector3 delta = new Vector3(0, 0, 0);
+
+				switch (faceName) {
+				case "Top": 
+					delta = new Vector3 (0, 1, 0);
+					break;
+				case "Bottom": 
+					delta = new Vector3 (0, -1, 0);
+					break;
+				case "Left": 
+					delta = new Vector3 (-1, 0, 0);
+					break;
+				case "Right": 
+					delta = new Vector3 (+1, 0, 0);
+					break;
+				case "Front": 
+					delta = new Vector3 (0, 0, -1);
+					break;
+				case "Back": 
+					delta = new Vector3 (0, 0, +1);
+					break;
+				}
+
+				Vector3 newPos = hit.transform.parent.gameObject.transform.position + delta;
+				if ((player.transform.position.x < newPos.x + 0.5f && player.transform.position.x > newPos.x - 0.5f) &&
+				    (player.transform.position.y < newPos.y + 0.5f && player.transform.position.y > newPos.y - 0.5f) &&
+				    (player.transform.position.z < newPos.z + 0.5f && player.transform.position.z > newPos.z - 0.5f)) {
+					// Debug.Log ("Inside");
+					return;
+				}
+		
+				Block newBlock = new Dirt (true, newPos);
+				worldBlocks [newPos] = newBlock;
+				newBlock.visible = true;
+				drawBlock (newBlock.type, newPos);
+
+				drawAroundInvisibleBlocks (newPos);
+
+			}
+		}
+	}
+}
--- Assets/Scripts/Generate_Landscape.cs
+++ Assets/Scripts/Generate_Landscape.cs
@@ -2,217 +2,91 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public class Tile {
+	public float creationTime;
+	public GameObject chunkObject;
 
-public enum Type {GRASS, SAND, SNOW};
-
-public class Block {
-	public Type type;
-	public bool visible;
-	public Vector3 pos;
-
-	public GameObject front;
-	public GameObject back;
-	public GameObject left;
-	public GameObject right;
-	public GameObject top;
-	public GameObject bottom;
-
-
-	public Block(Type type, bool visible, Vector3 pos) {
-		this.type = type;
-		this.visible = visible;
-		this.pos = pos;
-
-
-		front = null;
-		back = null;
-		left = null;
-		right = null;
-		top = null;
-		bottom = null;
-	}
-
-	public void addFaces( GameObject front, 
-						  GameObject back,
-						  GameObject left,
-						  GameObject right,
-						  GameObject top,
-						  GameObject bottom) {
-		this.front = front;
-		this.back = back;
-		this.left = left;
-		this.right = right;
-		this.top = top;
-		this.bottom = bottom;
-	}
-
-	public void disableInvisibleFaces(Dictionary<Vector3, Block> chunk) {
-		front.SetActive (true);
-		back.SetActive (true);
-		left.SetActive (true);
-		right.SetActive (true);
-		top.SetActive (true);
-		bottom.SetActive (true);
-
-
-		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y, (int)pos.z - 1))) {
-			front.SetActive(false);
-		}
-		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y, (int)pos.z + 1))) {
-			back.SetActive(false);
-		}
-		if (chunk.ContainsKey (new Vector3 ((int)pos.x + 1, (int)pos.y, (int)pos.z))) {
-			// right.GetComponent<Renderer> ().enabled = false;
-			// Destroy(right);
-			right.SetActive(false);
-		}
-		if (chunk.ContainsKey (new Vector3 ((int)pos.x - 1, (int)pos.y, (int)pos.z))) {
-			// left.GetComponent<Renderer> ().enabled = false;
-			// Destroy(left);
-			left.SetActive(false);
-		}
-		if (chunk.ContainsKey (new Vector3 ((int)pos.x, (int)pos.y - 1, (int)pos.z))) {
-			// bottom.GetComponent<Renderer> ().enabled = false;
-			// Destroy(bottom);
-			bottom.SetActive(false);
-		}
+	public Tile(GameObject chunkObject, float creationTime) {
+		this.chunkObject = chunkObject;
+		this.creationTime = creationTime;
 	}
 }
 
-
 public class Generate_Landscape : MonoBehaviour {
-	public static int width = 25;
-	public static int depth = 25;
-	public static int height = 128;
-	public int heightScale = 20;
-	public float detailScale = 25.0f;
+	public GameObject player;
+	public GameObject chunkPrefab;
 
-	private GameObject grassBlock;
-	private GameObject snowBlock;
-	private GameObject sandBlock;
+	public static int chunkWidth = 24;
+	public static int chunkDepth = 24;
+	public static int chunkHeight = 128;
 
-	Dictionary<Vector3, Block> worldBlocks = new Dictionary<Vector3, Block>();
+	private Vector3 startPos;
+
+	private int planeSize = 6;
+	private int seed = 0;
+
+	Dictionary<Vector2, Tile> tiles = new Dictionary<Vector2, Tile>();
 
 	// Use this for initialization
 	void Start () {
-		grassBlock = GameObject.Find("Grass");
-		snowBlock = GameObject.Find("Snow");
-		sandBlock = GameObject.Find("Sand");
+		startPos = Vector3.zero;
 
-		int seed = (int)Network.time * 10;
+		this.seed = (int)Network.time * 10;
 
-		ArrayList visibleBlocks = new ArrayList ();
+		float updateTime = Time.realtimeSinceStartup;
 
-		for (int z = 0; z < depth; z++) {
-			for (int x = 0; x < width; x++) {
-				int y = (int)(Mathf.PerlinNoise ((x+seed)/detailScale, (z+seed)/detailScale) * heightScale);
-				Vector3 blockPos = new Vector3 (x, y, z);
+		player.transform.position = new Vector3 (0, 30, 0);
 
-				visibleBlocks.Add(blockPos);
-				createBlock (blockPos);
-				while (y > 0) {
-					y--; 
-					blockPos = new Vector3 (x, y, z);
-					createBlock (blockPos);
-				}
+		for (int x = -planeSize / 2; x < planeSize / 2; x++) {
+			for (int z = -planeSize / 2; z < planeSize / 2; z++) {
+				GameObject chunkClone = (GameObject)Instantiate (chunkPrefab, new Vector3 (x * chunkWidth, 0, z * chunkDepth), Quaternion.identity);
+				chunkClone.GetComponent<Generate_Chunk> ().startGeneratingChunk (seed, chunkWidth, chunkDepth, chunkHeight, x, z, player);
+
+				Tile c = new Tile (chunkClone, updateTime);
+				tiles.Add (new Vector2 (x, z), c);
+
 			}
 		}
-
-		foreach (Vector3 blockPos in visibleBlocks) {
-			Block block = worldBlocks [blockPos];
-			block.visible = true;
-			drawBlock (block.type, blockPos);
-		}
-	}
-
-	void createBlock(Vector3 blockPos) {
-		int y = (int)blockPos.y;
-		Block block;
-		if (y > 15) {
-			block = new Block (Type.SNOW, false, blockPos);
-		} else if (y > 5) {
-			block = new Block (Type.GRASS, false, blockPos);
-		} else {
-			block = new Block (Type.SAND, false, blockPos);
-		}
-
-		worldBlocks[blockPos] = block;
-	}
-
-	void drawBlock(Type type, Vector3 blockPos) {
-		GameObject clone = null;
-
-		switch (type) {
-		case Type.GRASS:
-			clone = (GameObject)Instantiate (grassBlock, blockPos, Quaternion.identity);
-			//clone.name = "grass";
-			break;
-		case Type.SAND:
-			clone = (GameObject)Instantiate (sandBlock, blockPos, Quaternion.identity);
-			//clone.name = "sand";
-			break;
-		case Type.SNOW:
-			clone = (GameObject)Instantiate (snowBlock, blockPos, Quaternion.identity);
-			//clone.name = "snow";
-			break;
-		}
-
-		clone.name = blockPos.ToString ();
-			
-		GameObject top = (GameObject)clone.transform.FindChild ("Top").gameObject;
-		GameObject bottom = (GameObject)clone.transform.FindChild ("Bottom").gameObject;
-		GameObject left = (GameObject)clone.transform.FindChild ("Left").gameObject;
-		GameObject right = (GameObject)clone.transform.FindChild ("Right").gameObject;
-		GameObject front = (GameObject)clone.transform.FindChild ("Front").gameObject;
-		GameObject back = (GameObject)clone.transform.FindChild ("Back").gameObject;
-
-		Block block = worldBlocks [blockPos];
-		block.addFaces (front, back, left, right, top, bottom);
-		block.disableInvisibleFaces (worldBlocks);
-
-	}
-
-	void drawInvisibleBlock(Vector3 blockPos) {
-		if (worldBlocks.ContainsKey (blockPos) == false)
-			return;
-
-
-		Block block = worldBlocks[blockPos];
-		Type type = block.type;
-		if (block.visible) {
-			block.disableInvisibleFaces (worldBlocks);
-			return;
-		}
-		block.visible = true; 
-		drawBlock (block.type, blockPos);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0));
-			if (Physics.Raycast (ray, out hit, 200.0f)) {
-				GameObject block = hit.collider.gameObject.transform.parent.gameObject;
-				Vector3 blockPos = block.transform.position;
+		int playerX = (int)(player.transform.position.x / chunkWidth);
+		int playerZ = (int)(player.transform.position.z / chunkDepth);
 
-				// this is the bottom block, don't delete it 
-				if ((int)blockPos.y == 0) return;
+		int xMove = (int)(playerX - startPos.x);
+		int zMove = (int)(playerZ - startPos.z);
 
-				worldBlocks.Remove(blockPos);
-				Destroy (block);
+		if (xMove > 0 || zMove > 0) {
+			float updateTime = Time.realtimeSinceStartup;
 
-				for (int x = -1; x <= 1; x++) {
-					for (int y = -1; y <= 1; y++) {
-						for(int z = -1; z <= 1; z++) {
-							if (!(x == 0 && y == 0 && z == 0)) {
-								Vector3 neighbour = new Vector3 ((int)blockPos.x + x, (int)blockPos.y + y, (int)blockPos.z + z);
-								drawInvisibleBlock (neighbour);
-							}
-						}
+			for (int x = -planeSize / 2; x < planeSize / 2; x++) {
+				for (int z = -planeSize / 2; z < planeSize / 2; z++) {
+					Vector2 chunkPos = new Vector2 (x + playerX, z + playerZ);
+					if (!tiles.ContainsKey (chunkPos)) {
+						GameObject chunkClone = (GameObject)Instantiate (chunkPrefab, new Vector3 ((x+playerX)*chunkWidth, 0, (z+playerZ)*chunkDepth), Quaternion.identity);
+						chunkClone.GetComponent<Generate_Chunk> ().startGeneratingChunk (seed, chunkWidth, chunkDepth, chunkHeight, x+playerX, z+playerZ, player);
+
+						Tile c = new Tile (chunkClone, updateTime);
+						tiles.Add (new Vector2 (x+playerX, z+playerZ), c);
+					} else {
+						tiles [chunkPos].creationTime = updateTime;
 					}
 				}
 			}
+
+
+			Dictionary<Vector2, Tile> newTiles = new Dictionary<Vector2, Tile>();
+
+			foreach (KeyValuePair<Vector2, Tile> entry in tiles) {
+				if (entry.Value.creationTime != updateTime) { // remove old terrain. TODO: save that generated terrain.
+					Destroy (entry.Value.chunkObject);
+				} else {
+					newTiles.Add (entry.Key, entry.Value);
+				}
+			}
+			tiles = newTiles;
+			startPos = new Vector3 (playerX, 0, playerZ);
 		}
 	}
 }
