6b81ef4598e4b4bb8f1fe1bcf23589968d033a2c
Yiyi Wang <shd101wyy@sina.com>
2016-02-19 00:58:34 -0600

    add right hand wave animation

---
+++ Assets/Scripts/RightHandAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightHandAnimation : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("wave right hand");
+			anim.Play ("RightHandWave", -1, 0f);
+		}
+	}
+}
